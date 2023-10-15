3154711e24385fe944b92b60b9ea8b36f4ec2ba7
Markus Klie <markus.klie@web.de>
2023-10-15 18:21:16 +0200

    Added support for execution in CGI mode

--- rc/do_harvest.py
+++ rc/do_harvest.py
@@ -5,7 +5,7 @@
 #
 # By: agnosis.be
 # Repo: gabc
-# File version: 1.0
+# File version: 1.1
 #
 # Purpose:
 # Get and save the top-10 words suggested by Google's search box for every letter from a to z.
@@ -13,11 +13,19 @@
 #
 # Usage:
 # For this script to run once a day, you may want to invoke it using crontab or the like.
+# When published as a CGI script on a web server, making a daily HTTP GET request to it does the same.
+# Whatever the method, repetitive runs on the same day do not cause any harm.
 #
 
 import sys
+import os
+
 sys.path.append('../inc')
 sys.path.append('../config')
+
+if 'REQUEST_METHOD' in os.environ:
+    # if used as CGI script, send HTTP headers
+    print('Content-Type: text/plain; charset=UTF-8\r\n')
 
 from harvest import Harvester
 from my_client_id import CLIENT_ID
